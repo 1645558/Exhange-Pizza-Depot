5854de38e4171189e6b5629d2453f34aa3dacce8
Rare Raptor <connorwtuncc@gmail.com>
2022-10-11 12:46:08 -0400

    redesigning navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,19 +5,29 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <>
-            <div className='header'>
-                <Link id='about-link' to='about'>
-                    About Us
-                </Link>
-                <Link id='locations-link' to='locations'>
-                    Our Locations
-                </Link>
-                <Link id='menu-link' to='menu'>
-                    Menu
-                </Link>
-                <Link id='contact-link' to='contact'>
-                    Contact Us
-                </Link>
+            <div className='nav'>
+                <ul>
+                    <li>
+                        <Link id='about-link' to='about'>
+                            About Us
+                        </Link>
+                    </li>
+                    <li>
+                        <Link id='locations-link' to='locations'>
+                            Our Locations
+                        </Link>
+                    </li>
+                    <li>
+                        <Link id='menu-link' to='menu'>
+                            Menu
+                        </Link>
+                    </li>
+                    <li>
+                        <Link id='contact-link' to='contact'>
+                            Contact Us
+                        </Link>
+                    </li>
+                </ul>
             </div>
             <img src='https://img.cdn4dd.com/cdn-cgi/image/fit=contain,format=auto/https://doordash-static.s3.amazonaws.com/media/restaurant/cover/TheExchangePizzaDepot213MatthewsNC.png' alt='logo' />
         </>
